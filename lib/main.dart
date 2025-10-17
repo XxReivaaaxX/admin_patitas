1ae3c7b2d71095c1a8ac5bd4fc734fcbf6397d8e
Deryan Felipe Varela Lopez <deryan765@gmail.com>
2025-10-16 21:55:01 -0500

    ccc

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
 import 'firebase_options.dart';
 
 void main() async {
