c062d524a8d4cf9e05a4c4a05ba3afec94a47d08
Deryan Felipe Varela Lopez <deryan765@gmail.com>
2025-10-22 15:42:12 -0500

    cc

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:admin_patitas/screens/dashboard_animals.dart';
 import 'package:admin_patitas/screens/animals_screen.dart';
 import 'package:admin_patitas/screens/login_screen.dart';
+import 'package:admin_patitas/screens/screen_splash.dart'; 
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
- 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(AdminPatitasApp());
 }
- 
+
 class AdminPatitasApp extends StatelessWidget {
   const AdminPatitasApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Refugio de Animales - AdminPatitas',
       theme: ThemeData(primarySwatch: Colors.teal),
-      initialRoute: '/login',
+      initialRoute: '/', // 👈 Splash como ruta inicial
       routes: {
+        '/': (context) => SplashScreen(), // 👈 SplashScreen como primera pantalla
         '/login': (context) => LoginScreen(),
         '/dashboardAnimals': (context) => DashboardAnimal(),
         '/animalScreen': (context) => AnimalScreen(),
---
+++ lib/screens/screen_splash.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset('assets/png', width: 150),
+            SizedBox(height: 20),
+            Text(
+              'Refugio de Animales - AdminPatitas',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
