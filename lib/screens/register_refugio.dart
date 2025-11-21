e984b4a8722c1fc9f2de84787bea74837b643247
Deryan Felipe Varela Lopez <deryan765@gmail.com>
2025-11-20 22:33:25 -0500

    register_refugio

--- lib/screens/register_refugio.dart
+++ lib/screens/register_refugio.dart
@@ -38,7 +38,7 @@ class _RegisterRefugioState extends State<RegisterRefugio> {
   }
 
   void registerRefugio() async {
-    const url = "https://api-patitas-production-e508.up.railway.app/";
+    const url = "https://api-patitas-production-e508.up.railway.app/registro-animal";
     final uri = Uri.parse(url);
 
     try {
